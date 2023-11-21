13bba83c789e2733b2901f9a91e7306f70ac1cc1
Bob McAleavey <bobmcaleavey@gmail.com>
2023-11-21 14:42:08 -0500

    feat: update create-problem script to initialize test file

--- scripts/create-problem.js
+++ scripts/create-problem.js
@@ -15,6 +15,12 @@ const title = await new Promise((resolve) =>
 
 const path = `${day}-${title.toLowerCase().split(" ").join("-")}`;
 
+const writeFile = (path, content) => {
+  const writeStream = fs.createWriteStream(path);
+  writeStream.write(content);
+  writeStream.end();
+}
+
 readline.close();
 
 readline.on("close", () => process.exit(0));
@@ -22,18 +28,33 @@ readline.on("close", () => process.exit(0));
 if (!fs.existsSync(`./${path}`)) {
   fs.mkdirSync(`./${path}`, { recursive: true });
 
-  const readmeWriteStream = fs.createWriteStream(`./${path}/README.md`);
-  readmeWriteStream.write(`# ${day} - ${title}`);
-  readmeWriteStream.end();
+  const files = [
+    {
+      path: `./${path}/index.test.js`, content: [
+        `import { describe, it, expect } from 'vitest';`,
+        `import { input } from './input';`,
+        `import { solution } from './solution';`,
+        '',
+        `describe('Part One', () => {`,
+        `\tit('Example', () => {`,
+        `\t\tlet example = \`\`;`,
+        `\t\texpect(solution(example)).toBe(1);`,
+        `\t})`,
+        '',
+        `\tit('User Puzzle Input', () => {`,
+        `\t\texpect(solution(input)).toBe(1);`,
+        `\t})`,
+        `})`
+      ].join('\r')
+    },
+    { path: `./${path}/input.js`, content: `export const input = \`\`;` },
+    { path: `./${path}/README.md`, content: `# ${day} - ${title}` },
+    { path: `./${path}/solution.js`, content: `export const solution = (input = '') => {\r\treturn 1;\r}` },
+  ]
 
-  const inputWriteStream = fs.createWriteStream(`./${path}/input.js`);
-  inputWriteStream.write(`export default \`\`;`);
-  inputWriteStream.end();
-
-  const solutionWriteStream = fs.createWriteStream(`./${path}/solution.js`);
-  solutionWriteStream.write(`// import input from './input.js';\n\n`);
-  solutionWriteStream.write(`const input = \`\`;`);
-  solutionWriteStream.end();
+  files.map((file) => {
+    writeFile(file.path, file.content)
+  })
 
   let readmeContents = await new Promise((resolve) =>
     fs.readFile("./README.md", "utf8", (err, data) => resolve(data))
